319e3fb0ad34564aa6c14a9a8f3a1f579596aeaa
Kumao-sennsei <gooday.jukucafe@gmail.com>
2025-07-18 14:29:05 +0900

    Update index.js

--- index.js
+++ index.js
@@ -100,5 +100,7 @@ app.post('/webhook', middleware(lineConfig), (req, res) => {
 // ヘルスチェック
 app.get('/', (_req, res) => res.send('OK'));
 
-const PORT = process.env.PORT || 8080;
+- const PORT = 8080;
++ const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
